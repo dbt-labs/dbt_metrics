c3fff85f233ad21ebbdc14a34b181428361c7039
Joel Labes <joel.labes@dbtlabs.com>
2022-01-14 14:38:00 +1300

    Remove redundant files and add to gitignore

--- models/dim_slack_users.sql
+++
@@ -1,5 +0,0 @@
-select 1 as user_id, '2021-01-01 14:18:27' as joined_at, true as is_active_past_quarter, true as has_messaged
-union all 
-select 2 as user_id, '2021-02-03 17:18:55' as joined_at, false as is_active_past_quarter, true as has_messaged
-union all 
-select 3 as user_id, '2021-04-01 11:01:28' as joined_at, false as is_active_past_quarter, false as has_messaged
--- models/fct_orders.sql
+++
@@ -1,8 +0,0 @@
-
-select *,
-    case
-        when order_total < 50 then 'small'
-        else 'large'
-    end as order_total_band
-
-from PARTNER_MODE.RAW.ORDERS
