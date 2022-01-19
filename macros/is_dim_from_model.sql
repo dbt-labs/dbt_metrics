95ac75838f11b89bc63ce040ab659ba9beb5eea0
Joel Labes <joel.labes@dbtlabs.com>
2022-01-19 19:47:10 +1300

    Protect against missing meta configs

--- macros/is_dim_from_model.sql
+++ macros/is_dim_from_model.sql
@@ -1,5 +1,10 @@
 {% macro is_dim_from_model(metric, dim_name) %}
     {% if execute %}
+        -- For now, time dimensions have to be encoded in the meta tag. 
+        -- If there's no meta config, then assume all dimensions belong to the main model.
+        {% if not metric['meta']['dimensions'] %}
+            {% do return(True) %}
+        {% endif %}
 
         {% set model_dims = metric['meta']['dimensions'][0]['columns'] %}
         {% do return (dim_name in model_dims) %}
