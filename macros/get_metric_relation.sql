52f9885b56f8c737e7e8672ec3f4873000a027bd
Joel Labes <joel.labes@dbtlabs.com>
2022-02-08 08:32:32 +1300

    Add comment expanding on problem

--- macros/get_metric_relation.sql
+++ macros/get_metric_relation.sql
@@ -1,6 +1,5 @@
 {% macro get_metric_relation(ref_name) %}
     {% if execute %}
-        /* TODO: How do we properly handle refs[0] for the metric's model, and the ref() syntax for the calendar table? */
         {% set model_ref_node = graph.nodes.values() | selectattr('name', 'equalto', ref_name[0]) | first %}
         {% set relation = api.Relation.create(
             database = model_ref_node.database,
@@ -15,6 +14,42 @@
 {% endmacro %}
 
 {% macro get_metric_calendar(ref_name) %}
-    --TODO: this is HORRID.
+    /*
+        TODO: this is HORRID.
+        Short version: How do we properly handle refs[0] for the metric's model, and the ref() syntax for the calendar table? 
+    */
+
+    /*
+        Long version: even though the metric yml file has its model as a full ref
+
+        - name: slack_joiners
+        model: ref('dim_slack_users_2')
+
+        the refs array from the graph contains just the string, inside a second array:
+
+        {
+        "fqn":["joel_sandbox","metrics","slack_joiners"],
+        "unique_id":"metric.joel_sandbox.slack_joiners",
+        "time_grains":["day", "week", "month"],
+        "dimensions":["has_messaged"],
+        "resource_type":"metric",
+        "refs":[
+            [
+                "dim_slack_users_2"
+            ]
+        ],
+        "created_at":1642578505.5324879
+        }
+
+
+        Whereas the calendar variable:
+        vars:
+            dbt_metrics_calendar_model: ref('all_days_extended_2')
+
+        comes through as the entire ref string (it hasn't been parsed or processed yet). 
+        This splits on the ' character, takes the second element, and wraps it inside an array, 
+        to have the same shape as get_metric_relation expects,
+        which is written to expect the metric's `model`.
+    */
     {% do return(metrics.get_metric_relation([(ref_name.split("'")[1])])) %}
 {% endmacro %}
