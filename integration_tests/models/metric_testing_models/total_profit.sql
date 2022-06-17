19660fb6044500c579e559bb637e31061920635f
Callum McCann <cmccann51@gmail.com>
2022-06-17 10:43:33 -0500

    adding more integration tests

--- integration_tests/models/metric_testing_models/total_profit.sql
+++ integration_tests/models/metric_testing_models/total_profit.sql
@@ -2,5 +2,7 @@ select *
 from 
 {{ metrics.metric(metric('total_profit'), 
     grain='day', 
-    dimensions=['had_discount','order_country','is_weekend']) 
+    dimensions=['had_discount','order_country','is_weekend'],
+    start_date = '2022-01-01',
+    end_date = '2022-01-10') 
 }}
