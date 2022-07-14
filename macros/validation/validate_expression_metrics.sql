2ae95b0e0d2630946ffcd21e5a6933ec03c5d5bb
Callum McCann <cmccann51@gmail.com>
2022-07-14 16:35:44 -0500

    let there be macros

---
+++ macros/validation/validate_expression_metrics.sql
@@ -0,0 +1,13 @@
+{% macro validate_expression_metrics(full_set) %}
+
+    {# We loop through the full set here to ensure that metrics that aren't listed 
+    as expression are note dependent on another metric.  #}
+
+    {% for metric_name in full_set %}
+        {% set metric_relation = metric(metric_name)%}
+        {% if metric_relation.type != "expression" and metric_relation.metrics | length > 0 %}
+            {%- do exceptions.raise_compiler_error("The metric " ~ metric_relation.name ~ " was not an expression and dependent on another metric. This is not currently supported - if this metric depends on another metric, please change the type to expression.") %}
+        {%- endif %}
+    {% endfor %}
+
+{% endmacro %}
