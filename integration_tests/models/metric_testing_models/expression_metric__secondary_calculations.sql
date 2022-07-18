05ad0ca202e434bbf58187be822379da680bd74e
Callum McCann <cmccann51@gmail.com>
2022-07-18 14:15:32 -0500

    removing failing integration test expression

--- integration_tests/models/metric_testing_models/expression_metric__secondary_calculations.sql
+++ integration_tests/models/metric_testing_models/expression_metric__secondary_calculations.sql
@@ -3,9 +3,6 @@
   {{ metrics.calculate(
       metric('ratio_metric'), 
       grain='day', 
-      dimensions=['had_discount'], 
-      secondary_calculations=[
-        metrics.rolling(aggregate="average", interval=7)
-      ] 
+      dimensions=['had_discount']
       )
   }}
