b3266bf79e045bd6ce5be94aca86fc2cccb2cc21
Callum McCann <cmccann51@gmail.com>
2022-09-11 15:22:27 -0500

    updating for main

--- integration_tests/models/metric_testing_models/base_sum_metric.sql
+++ integration_tests/models/metric_testing_models/base_sum_metric.sql
@@ -1,7 +1,7 @@
 select *
 from 
 {{ dbt_metrics.calculate(
-    metric('base_sum_metric'), 
-    grain='day', 
+    [metric('base_sum_metric'),metric('base_test_metric')], 
+    grain='all_time', 
     dimensions=['had_discount','is_weekend']) 
 }}
