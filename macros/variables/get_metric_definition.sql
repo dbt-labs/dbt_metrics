ac6df05d9d1fb5848d47f25739bd9df768e2573d
Callum McCann <cmccann51@gmail.com>
2022-09-22 14:46:54 -0500

    changes

--- macros/variables/get_metric_definition.sql
+++ macros/variables/get_metric_definition.sql
@@ -2,48 +2,31 @@
 
     {% set metrics_dictionary_dict = {} %}
 
+    {% do metrics_dictionary_dict.update({'name': metric_definition.name})%}
+    {% do metrics_dictionary_dict.update({'calculation_method': metric_definition.calculation_method})%}
+    {% do metrics_dictionary_dict.update({'timestamp': metric_definition.timestamp})%}
+    {% do metrics_dictionary_dict.update({'time_grains': metric_definition.time_grains})%}
+    {% do metrics_dictionary_dict.update({'dimensions': metric_definition.dimensions})%}
+    {% do metrics_dictionary_dict.update({'filters': metric_definition.filters})%}
+    {% if metric_definition.calculation_method != 'derived' %}
+        {% set metric_model_name = metrics.get_metric_model_name(metric_model=metric_definition.model) %}
+        {% do metrics_dictionary_dict.update({'metric_model': metrics.get_model_relation(metric_model_name, metric_name)}) %}
+    {% endif %}
+
+    {# Behavior specific to develop #}
     {% if metric_definition is mapping %}
-
-        {# THIS IS THE PATH FOR DEVELOP #}
-
-        {% do metrics_dictionary_dict.update({'name': metric_definition.name})%}
-        {% do metrics_dictionary_dict.update({'calculation_method': metric_definition.calculation_method})%}
         {# We need to do some cleanup for metric parsing #}
         {% set metric_expression = metric_definition.expression | replace("metric(","") | replace(")","") | replace("{{","") | replace("}}","")  | replace("'","") | replace('"',"")  %}
         {% do metrics_dictionary_dict.update({'expression': metric_expression})%} 
-        {% do metrics_dictionary_dict.update({'timestamp': metric_definition.timestamp})%}
-        {% do metrics_dictionary_dict.update({'time_grains': metric_definition.time_grains})%}
-        {% do metrics_dictionary_dict.update({'dimensions': metric_definition.dimensions})%}
-        {% do metrics_dictionary_dict.update({'filters': metric_definition.filters})%}
-
-        {% if metric_definition.calculation_method != 'derived' %}
-            {% set metric_model_name = metrics.get_metric_model_name(metric_model=metric_definition.model) %}
-            {% do metrics_dictionary_dict.update({'metric_model': metrics.get_model_relation(metric_model_name, metric_name)}) %}
-        {% endif %}
-
         {% if metric_definition.window %}
             {% do metrics_dictionary_dict.update({'window': metric_definition.window}) %}
         {% else %}
             {% do metrics_dictionary_dict.update({'window': None}) %}
         {% endif %}
-
+    {# Behavior specific to calculate #}
     {% else %}
-
-        {# THIS IS THE PATH FOR THE METRIC OBJECT #}
-        {% do metrics_dictionary_dict.update({'name': metric_definition.name})%}
-        {% do metrics_dictionary_dict.update({'calculation_method': metric_definition.calculation_method})%}
         {% do metrics_dictionary_dict.update({'expression': metric_definition.expression})%} 
-        {% do metrics_dictionary_dict.update({'timestamp': metric_definition.timestamp})%}
-        {% do metrics_dictionary_dict.update({'time_grains': metric_definition.time_grains})%}
-        {% do metrics_dictionary_dict.update({'dimensions': metric_definition.dimensions})%}
-        {% do metrics_dictionary_dict.update({'filters': metric_definition.filters})%}
         {% do metrics_dictionary_dict.update({'window': metric_definition.window})%}
-
-        {% if metric_definition.calculation_method != 'derived' %}
-            {% set metric_model_name = metrics.get_metric_model_name(metric_model=metric_definition.model) %}
-            {% do metrics_dictionary_dict.update({'metric_model': metrics.get_model_relation(metric_model_name, metric_name)}) %}
-        {% endif %}
-
     {% endif %}
 
     {% do return(metrics_dictionary_dict) %}
--- macros/variables/get_metrics_dictionary.sql
+++ macros/variables/get_metrics_dictionary.sql
@@ -2,27 +2,16 @@
 
     {% set metrics_dictionary = {} %}
 
-    {% if develop_yml is not none %}
-
-        {% for metric_name in metric_tree.full_set %}
-            {% set metric_definition = develop_yml[metric_name]%}
-            {% set single_metric_dict = metrics.get_metric_definition(metric_definition) %}
-            {% do metrics_dictionary.update({metric_name:{}})%}
-            {% do metrics_dictionary.update({metric_name:single_metric_dict})%}
-        {% endfor %}
-    
-    {% else %}
-
-        {% for metric_name in metric_tree.full_set %}
-
-            {% set dict_metric = metrics.get_metric_relation(metric_name) %}
-            {% set single_metric_dict = metrics.get_metric_definition(dict_metric) %}
-            {% do metrics_dictionary.update({metric_name:{}})%}
-            {% do metrics_dictionary.update({metric_name:single_metric_dict})%}
-
-        {% endfor %}
-        
-    {% endif %}
+    {% for metric_name in metric_tree.full_set %}
+        {% if develop_yml is not none %}
+            {% set metric_object = develop_yml[metric_name]%}
+        {% else %}
+            {% set metric_object = metrics.get_metric_relation(metric_name) %}
+        {% endif %}
+        {% set metric_definition = metrics.get_metric_definition(metric_object) %}
+        {% do metrics_dictionary.update({metric_name:{}})%}
+        {% do metrics_dictionary.update({metric_name:metric_definition})%}
+    {% endfor %}
 
     {% do return(metrics_dictionary) %}
 
