629931a5b07d4bb7d7b3b2737110e647f27ba53f
Dave Connors <dave.connors@fishtownanalytics.com>
2022-09-23 14:34:43 -0500

    back to jerco's original code

--- macros/sql_gen/build_metric_sql.sql
+++ macros/sql_gen/build_metric_sql.sql
@@ -1,6 +1,6 @@
 {%- macro build_metric_sql(metric_dictionary, grain, dimensions, secondary_calculations, start_date, end_date, calendar_tbl, relevant_periods, calendar_dimensions, dimensions_provided) %}
     
-    {%- set treat_null_values_as_zero = metric_dictionary.get("config").get("treat_null_values_as_zero") if metric_dictionary.get("config").get("treat_null_values_as_zero") else True  -%}
+    {%- set treat_null_values_as_zero = metric_dictionary.get("config").get("treat_null_values_as_zero", True)  -%}
     {#- This is the SQL Gen part - we've broken each component out into individual macros -#}
     {#- We broke this out so it can loop for composite metrics -#}
     {{ metrics.gen_aggregate_cte(
