cf52895cc6ad743a0c36768d4151ed6e7815d890
Callum McCann <101437052+callum-mcdata@users.noreply.github.com>
2022-10-06 16:52:40 -0500

    Revert "Fixing Redshift Order By Bug"

--- macros/calculate.sql
+++ macros/calculate.sql
@@ -96,6 +96,4 @@
         relevant_periods=relevant_periods
     ) %}
     ({{ sql }}) metric_subq
-    {{- metrics.gen_order_by(grain, dimensions, calendar_dimensions, relevant_periods) -}}
-
 {%- endmacro %}
