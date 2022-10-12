63a3ee6000e8a15d23a163454cbecf663b69e8a4
Callum McCann <cmccann51@gmail.com>
2022-10-12 11:23:21 -0500

    updating custom cal

--- tests/functional/fixtures.py
+++ tests/functional/fixtures.py
@@ -46,7 +46,7 @@ purchased_at,payment_type,payment_total
 custom_calendar_sql = """
 {{ config(materialized='table') }}
 with days as (
-    {{ dbt_utils.date_spine(
+    {{ metrics.metric_date_spine(
     datepart="day",
     start_date="cast('2010-01-01' as date)",
     end_date="cast('2030-01-01' as date)"
