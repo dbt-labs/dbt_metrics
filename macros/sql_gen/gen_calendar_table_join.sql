2ddbe813d981ba0307ee50fca7d6bb0f0b30b8e3
Callum McCann <cmccann51@gmail.com>
2022-11-16 15:29:56 -0600

    base model copy paste error

--- macros/sql_gen/gen_calendar_table_join.sql
+++ macros/sql_gen/gen_calendar_table_join.sql
@@ -6,10 +6,10 @@
 
     left join {{calendar_tbl}} calendar_table
     {% if metric_dictionary.window is not none %}
-        on cast(calendar_table.{{metric_dictionary.timestamp}} as date) > dateadd({{metric_dictionary.window.period}}, -{{metric_dictionary.window.count}}, calendar_table.date_day)
-        and cast(calendar_table.{{metric_dictionary.timestamp}} as date) <= calendar_table.date_day
+        on cast(base_model.{{metric_dictionary.timestamp}} as date) > dateadd({{metric_dictionary.window.period}}, -{{metric_dictionary.window.count}}, calendar_table.date_day)
+        and cast(base_model.{{metric_dictionary.timestamp}} as date) <= calendar_table.date_day
     {% else %}
-        on cast(calendar_table.{{metric_dictionary.timestamp}} as date) = calendar_table.date_day
+        on cast(base_model.{{metric_dictionary.timestamp}} as date) = calendar_table.date_day
     {% endif %}
 
 {% endmacro %}
@@ -18,10 +18,10 @@
 
     left join {{calendar_tbl}} calendar_table
     {% if metric_dictionary.window is not none %}
-        on cast(calendar_table.{{metric_dictionary.timestamp}} as date) > date_sub(calendar_table.date_day, interval {{metric_dictionary.window.count}} {{metric_dictionary.window.period}})
-        and cast(calendar_table.{{metric_dictionary.timestamp}} as date) <= calendar_table.date_day
+        on cast(base_model.{{metric_dictionary.timestamp}} as date) > date_sub(calendar_table.date_day, interval {{metric_dictionary.window.count}} {{metric_dictionary.window.period}})
+        and cast(base_model.{{metric_dictionary.timestamp}} as date) <= calendar_table.date_day
     {% else %}
-        on cast(calendar_table.{{metric_dictionary.timestamp}} as date) = calendar_table.date_day
+        on cast(base_model.{{metric_dictionary.timestamp}} as date) = calendar_table.date_day
     {% endif %}
 
 {% endmacro %}
