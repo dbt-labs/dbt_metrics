277850a3833e803b595f3e32410b0461ef2328b0
Callum McCann <cmccann51@gmail.com>
2022-11-17 11:19:49 -0600

    moving filters

--- macros/sql_gen/gen_base_query.sql
+++ macros/sql_gen/gen_base_query.sql
@@ -44,26 +44,6 @@
 
         where 1=1
         
-        {#- metric start/end dates also applied here to limit incoming data -#}
-        {% if start_date or end_date %}
-            and (
-            {% if start_date and end_date -%}
-                cast({{metric_dictionary.timestamp}} as date) >= cast('{{ start_date }}' as date)
-                and cast({{metric_dictionary.timestamp}} as date) <= cast('{{ end_date }}' as date)
-            {%- elif start_date and not end_date -%}
-                cast({{metric_dictionary.timestamp}} as date) >= cast('{{ start_date }}' as date)
-            {%- elif end_date and not start_date -%}
-                cast({{metric_dictionary.timestamp}} as date) <= cast('{{ end_date }}' as date)
-            {%- endif %} 
-            )
-        {% endif -%} 
-        {#- metric filter clauses... -#}
-        {% if metric_dictionary.filters %}
-            and (
-                {% for filter in metric_dictionary.filters -%}
-                    {%- if not loop.first -%} and {% endif %}{{ filter.field }} {{ filter.operator }} {{ filter.value }}
-                {% endfor -%}
-            )
-        {% endif -%}
+        {{ metrics.gen_filters(metric_dictionary, start_date, end_date) }}
 
 {%- endmacro -%}
