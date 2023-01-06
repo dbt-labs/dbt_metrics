851e30399312e66ac5a5e6b07fb5def105030546
Callum McCann <cmccann51@gmail.com>
2023-01-06 16:35:31 -0600

    fixing bigquery tests

--- tests/functional/develop/test_develop.py
+++ tests/functional/develop/test_develop.py
@@ -8,14 +8,15 @@ from tests.functional.fixtures import (
     fact_orders_source_csv,
     fact_orders_sql,
     fact_orders_yml,
+    custom_calendar_sql
 )
 
-# models/develop_metric.sql
-develop_metric_sql = """
+# models/develop_metric_monthly.sql
+develop_metric_monthly_sql = """
 {% set my_metric_yml -%}
 
 metrics:
-  - name: develop_metric
+  - name: develop_metric_monthly
     model: ref('fact_orders')
     label: Total Discount ($)
     timestamp: order_date
@@ -33,50 +34,50 @@ metrics:
 select * 
 from {{ metrics.develop(
         develop_yml=my_metric_yml,
-        metric_list='develop_metric',
+        metric_list=['develop_metric_monthly'],
         grain='month'
         )
     }}
 """
 
-# models/develop_metric.yml
-develop_metric_yml = """
+# models/develop_metric_monthly.yml
+develop_metric_monthly_yml = """
 version: 2 
 models:
-  - name: develop_metric
+  - name: develop_metric_monthly
     tests: 
       - metrics.metric_equality:
-          compare_model: ref('develop_metric__expected')
+          compare_model: ref('develop_metric_monthly__expected')
 
 """
 
 # seeds/develop_metric__expected.csv
 if os.getenv('dbt_target') == 'snowflake':
-    develop_metric__expected_csv = """
-date_month,develop_metric
+    develop_metric_monthly__expected_csv = """
+date_month,develop_metric_monthly
 2022-01-01,1.000000
 2022-02-01,1.333333
 """.lstrip()
 else:
-    develop_metric__expected_csv = """
-date_month,develop_metric
+    develop_metric_monthly__expected_csv = """
+date_month,develop_metric_monthly
 2022-01-01,1.00000000000000000000
 2022-02-01,1.3333333333333333
 """.lstrip()
 
-# seeds/develop_metric___expected.yml
+# seeds/develop_metric_monthly___expected.yml
 if os.getenv('dbt_target') == 'bigquery':
-    develop_metric__expected_yml = """
+    develop_metric_monthly__expected_yml = """
 version: 2
 seeds:
   - name: develop_metric__expected
     config:
       column_types:
         date_month: date
-        develop_metric: FLOAT64
+        develop_metric_monthly: FLOAT64
 """.lstrip()
 else: 
-    develop_metric__expected_yml = """"""
+    develop_metric_monthly__expected_yml = """"""
 
 class TestDevelopMonthlyMetric:
     # configuration in dbt_project.yml
@@ -102,8 +103,8 @@ class TestDevelopMonthlyMetric:
     def seeds(self):
         return {
             "fact_orders_source.csv": fact_orders_source_csv,
-            "develop_metric__expected.csv": develop_metric__expected_csv,
-            "develop_metric__expected.yml": develop_metric__expected_yml
+            "develop_metric_monthly__expected.csv": develop_metric_monthly__expected_csv,
+            "develop_metric_monthly__expected.yml": develop_metric_monthly__expected_yml
         }
 
     # everything that goes in the "models" directory
@@ -112,11 +113,11 @@ class TestDevelopMonthlyMetric:
         return {
             "fact_orders.sql": fact_orders_sql,
             "fact_orders.yml": fact_orders_yml,
-            "develop_metric.sql": develop_metric_sql,
-            "develop_metric.yml": develop_metric_yml
+            "develop_metric_monthly.sql": develop_metric_monthly_sql,
+            "develop_metric_monthly.yml": develop_metric_monthly_yml
         }
 
-    def test_build_completion(self,project,):
+    def test_develop_monthly_metric(self,project,):
         # running deps to install package
         results = run_dbt(["deps"])
 
@@ -126,6 +127,7 @@ class TestDevelopMonthlyMetric:
 
         # initial run
         results = run_dbt(["run"])
+        # breakpoint()
         assert len(results) == 3
 
         # test tests
@@ -142,7 +144,7 @@ develop_multiple_metrics_sql = """
 {% raw %}
 
 metrics:
-  - name: develop_metric
+  - name: develop_metric_multiple
     model: ref('fact_orders')
     label: Total Discount ($)
     timestamp: order_date
@@ -153,22 +155,22 @@ metrics:
       - had_discount
       - order_country
 
-  - name: derived_metric
+  - name: derived_metric_multiple
     label: Total Discount ($)
     timestamp: order_date
     time_grains: [day, week, month]
     calculation_method: derived
-    expression: "{{ metric('develop_metric') }} - 1 "
+    expression: "{{ metric('develop_metric_multiple') }} - 1 "
     dimensions:
       - had_discount
       - order_country
 
-  - name: some_other_metric_not_using
+  - name: some_other_metric_not_using_multiple
     label: Total Discount ($)
     timestamp: order_date
     time_grains: [day, week, month]
     calculation_method: derived
-    expression: "{{ metric('derived_metric') }} - 1 "
+    expression: "{{ metric('derived_metric_multiple') }} - 1 "
     dimensions:
       - had_discount
       - order_country
@@ -179,7 +181,7 @@ metrics:
 select * 
 from {{ metrics.develop(
         develop_yml=my_metric_yml,
-        metric_list=['derived_metric'],
+        metric_list=['derived_metric_multiple'],
         grain='month'
         )
     }}
@@ -199,13 +201,13 @@ models:
 # seeds/develop_multiple_metrics___expected.yml
 if os.getenv('dbt_target') == 'snowflake':
     develop_multiple_metrics__expected_csv = """
-date_month,develop_metric,derived_metric
+date_month,develop_metric_multiple,derived_metric_multiple
 2022-02-01,1.333333,0.333333
 2022-01-01,1.0,0.0
 """.lstrip()
 else: 
     develop_multiple_metrics__expected_csv = """
-date_month,develop_metric,derived_metric
+date_month,develop_metric_multiple,derived_metric_multiple
 2022-02-01,1.3333333333333333,0.33333333333333326
 2022-01-01,1.0,0.0
 """.lstrip()
@@ -219,7 +221,8 @@ seeds:
     config:
       column_types:
         date_month: date
-        develop_metric: FLOAT64
+        develop_metric_multiple: FLOAT64
+        derived_metric_multiple: FLOAT64
 """.lstrip()
 else: 
     develop_multiple_metrics__expected_yml = """"""
@@ -262,7 +265,7 @@ class TestDevelopMultipleMetrics:
             "develop_multiple_metrics.yml": develop_multiple_metrics_yml
         }
 
-    def test_build_completion(self,project,):
+    def test_develop_multiple_metrics(self,project,):
         # running deps to install package
         results = run_dbt(["deps"])
 
@@ -282,19 +285,12 @@ class TestDevelopMultipleMetrics:
         result_statuses = sorted(r.status for r in results)
         assert result_statuses == ["pass"]
 
-# our file contents
-from tests.functional.fixtures import (
-    fact_orders_source_csv,
-    fact_orders_sql,
-    fact_orders_yml,
-    custom_calendar_sql
-)
 
-# models/develop_metric.sql
-develop_metric_sql = """
+# models/develop_metric_window.sql
+develop_metric_window_sql = """
 {% set my_metric_yml -%}
 metrics:
-  - name: develop_metric
+  - name: develop_metric_window
     model: ref('fact_orders')
     label: Total Discount ($)
     timestamp: order_date
@@ -311,25 +307,25 @@ metrics:
 select * 
 from {{ metrics.develop(
         develop_yml=my_metric_yml,
-        metric_list='develop_metric',
+        metric_list=['develop_metric_window'],
         grain='week'
         )
     }}
 """
 
-# models/develop_metric.yml
-develop_metric_yml = """
+# models/develop_metric_window.yml
+develop_metric_window_yml = """
 version: 2 
 models:
-  - name: develop_metric
+  - name: develop_metric_window
     tests: 
       - metrics.metric_equality:
-          compare_model: ref('develop_metric__expected')
+          compare_model: ref('develop_metric_window__expected')
 """
 
 # seeds/develop_metric__expected.csv
-develop_metric__expected_csv = """
-date_week,develop_metric
+develop_metric_window__expected_csv = """
+date_week,develop_metric_window
 2022-01-10,2
 2022-01-17,3
 2022-01-24,4
@@ -368,7 +364,7 @@ class TestDevelopMetricWindow:
     def seeds(self):
         return {
             "fact_orders_source.csv": fact_orders_source_csv,
-            "develop_metric__expected.csv": develop_metric__expected_csv
+            "develop_metric_window__expected.csv": develop_metric_window__expected_csv
         }
 
     # everything that goes in the "models" directory
@@ -377,12 +373,12 @@ class TestDevelopMetricWindow:
         return {
             "fact_orders.sql": fact_orders_sql,
             "fact_orders.yml": fact_orders_yml,
-            "develop_metric.sql": develop_metric_sql,
-            "develop_metric.yml": develop_metric_yml,
+            "develop_metric_window.sql": develop_metric_window_sql,
+            "develop_metric_window.yml": develop_metric_window_yml,
             "custom_calendar.sql": custom_calendar_sql
         }
 
-    def test_build_completion(self,project,):
+    def test_develop_metric_window(self,project,):
         # running deps to install package
         results = run_dbt(["deps"])
 
@@ -402,12 +398,6 @@ class TestDevelopMetricWindow:
         result_statuses = sorted(r.status for r in results)
         assert result_statuses == ["pass"]
 
-# our file contents
-from tests.functional.fixtures import (
-    fact_orders_source_csv,
-    fact_orders_sql,
-    fact_orders_yml,
-)
 
 # models/develop_metric_dimension.sql
 develop_metric_dimension_sql = """
@@ -496,7 +486,7 @@ class TestDevelopMetricDimension:
             "develop_metric_dimension.yml": develop_metric_dimension_yml
         }
 
-    def test_build_completion(self,project,):
+    def test_develop_metric_dimension(self,project,):
         # running deps to install package
         results = run_dbt(["deps"])
 
@@ -516,12 +506,6 @@ class TestDevelopMetricDimension:
         result_statuses = sorted(r.status for r in results)
         assert result_statuses == ["pass"]
 
-# our file contents
-from tests.functional.fixtures import (
-    fact_orders_source_csv,
-    fact_orders_sql,
-    fact_orders_yml,
-)
 
 # models/testing_metric__develop.sql
 testing_metric_develop_sql = """
@@ -620,13 +604,21 @@ class TestDevelopMatchesCalculate:
         return {
             "fact_orders.sql": fact_orders_sql,
             "fact_orders.yml": fact_orders_yml,
+        }
+
+    # everything that goes in the "models" directory
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "fact_orders.sql": fact_orders_sql,
+            "fact_orders.yml": fact_orders_yml,
             "testing_metric_develop.sql": testing_metric_develop_sql,
             "testing_metric_develop.yml": testing_metric_develop_yml,
             "testing_metric_calculate.sql": testing_metric_calculate_sql,
             "testing_metric_calculate.yml": testing_metric_calculate_yml
         }
 
-    def test_build_completion(self,project,):
+    def test_develop_matches_calculate_second_run(self,project,):
         # running deps to install package
         results = run_dbt(["deps"])
 
