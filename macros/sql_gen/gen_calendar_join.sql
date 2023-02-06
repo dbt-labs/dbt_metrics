af6305246e57920b2bf702f9bc02ac203682a375
Robbert Hofman <robbert.hofman@dataminded.be>
2023-02-06 18:35:54 +0100

    added trino__gen_calendar_join

--- macros/sql_gen/gen_calendar_join.sql
+++ macros/sql_gen/gen_calendar_join.sql
@@ -41,3 +41,13 @@
             on cast(base_model.{{group_values.timestamp}} as date) = calendar.date_day
         {% endif -%}
 {% endmacro %}
+
+{% macro trino__gen_calendar_join(group_values) %}
+        left join calendar
+        {%- if group_values.window is not none %}
+            on cast(base_model.{{group_values.timestamp}} as date) > date_add('{{group_values.window.period}}', -{{group_values.window.count}}, calendar.date_day)
+            and cast(base_model.{{group_values.timestamp}} as date) <= calendar.date_day
+        {%- else %}
+            on cast(base_model.{{group_values.timestamp}} as date) = calendar.date_day
+        {% endif -%}
+{% endmacro %}
